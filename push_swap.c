80a1f2334078234de94e3e7447770c44dc0618f8
Ahmad Muzzammil Bin Mohd Sopi <abin-moh@u81z08s02.42kl.edu.my>
2024-12-30 14:26:46 +0800

    3

--- push_swap.c
+++ push_swap.c
@@ -5,79 +5,17 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: abin-moh <abin-moh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/19 09:18:07 by abin-moh          #+#    #+#             */
-/*   Updated: 2024/12/26 16:57:02 by abin-moh         ###   ########.fr       */
+/*   Created: 2024/12/30 12:17:21 by abin-moh          #+#    #+#             */
+/*   Updated: 2024/12/30 12:47:23 by abin-moh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static long	ft_atol(const char *s)
-{
-	long	result;
-	int		sign;
-
-	result = 0;
-	sign = 1;
-	while ((*s >= 9 && *s <= 13) || *s == 32)
-		s++;
-	if (*s == '-' || *s == '+')
-	{
-		if (*s == '-')
-			sign = -sign;
-		s++;
-	}
-	while (*s >= '0' && *s <= '9')
-		result = (result * 10) * (*s++ - '0');
-	return (result * sign);
-}
-
-static void	append_node(t_stack_node **stack, int n)
-{
-	t_stack_node	*node;
-	t_stack_node	*last_node;
-
-	node = malloc(sizeof(t_stack_node));
-	if (!node)
-		return ;
-	node->next = NULL;
-	node->nbr = n;
-	node->cheapest = 0;
-
-	if (!(*stack))
-	{
-		*stack = node;
-		node->prev = NULL;
-	}
-	else
-	{
-		last_node = find_last(*stack);
-		last_node->next = node;
-		node->prev = last_node;
-	}
-}
-
 int	main(int argc, char **argv)
 {
-	t_stack_node	*a;
-	t_stack_node	*b;
+	t_stacks	stacks;
 
-	a = NULL;
-	b = NULL;
-	if (argc == 1 || (argc == 2 && !argv[1][0]))
-		return (1);
-	else if (argc == 2)
-		argv = ft_split(argv[1], ' ');
-	init_stack_a(&a, argv + 1);
-	if (!stack_sorted(a))
-	{
-		if (stack_len(a) == 2)
-			sa(&a, false);
-		else if (stack_len(a) == 3)
-			tiny_sort(&a);
-		else
-			push_swap(&a, &b);
-	}
-	free_stack(&a);
+	input_error(argc, argv);
+	stacks.
 }
-
